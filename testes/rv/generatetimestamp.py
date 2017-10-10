58149b031ebd33f5bedd4b9672c11e7fcd403582
Jailson Dias <jcd2@cin.ufpe.br>
2017-10-10 12:25:16 -0300

    Inserindo teste do pitch

---
+++ testes/rv/generatetimestamp.py
@@ -0,0 +1,16 @@
+# import glob
+# from os import listdir
+# from os.path import isfile, join
+
+timestamp = 0.03332755 # tempo entre cada frame
+
+# fotos = glob.glob("rgb/*.png")
+# print (len(fotos))
+# fotos.sort()
+
+# f = open("rgb.txt", "w")
+
+# for foto in fotos:
+#     f.write(foto + "\n")
+
+f.close()
