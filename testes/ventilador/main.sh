478381f1f88521ca51b818ba5f8abd30fd0d440b
Jailson Dias <jcd2@cin.ufpe.br>
2017-10-12 19:29:59 -0300

    atualizando o arquivo main.sh

--- testes/ventilador/main.sh
+++ testes/ventilador/main.sh
@@ -24,12 +24,13 @@ if [ $img -eq 1 ] ; then
     if [ -e rgb ] ; then # verificando se a pasta rgb ja foi criada
         echo "Imagens já geradas, deseja gerar as imagens outra vez [S/n]?"
         read criar 
-    else
-        echo "Criando pasta rgb para colocar as imagens geradas"
-        mkdir rgb # criando pasta rgb caso ela nao ja esteja criada
     fi
     if [ ! "$criar" == "n" ] && [ ! "$criar" == "N" ] ; then
+        rm -rf rgb
+        echo "Criando pasta rgb para colocar as imagens geradas"
+        mkdir rgb # criando pasta rgb caso ela nao ja esteja criada
         ffmpeg -i "$video" -vf fps=30 rgb/%06d.png # gerando as imagens a partir do video
+        criar="S"
     fi
 fi
 ############################################################################
@@ -40,9 +41,8 @@ fi
 
 
 ################ Gerando arquivo rgb.txt a partir das imagens ################
-criar=""
 
-if [ -e rgb.txt ] ; then # verificando se o arquivo rgb.txt ja foi criado
+if [ -e rgb.txt ] && [ ! "$criar" == "S" ] ; then # verificando se o arquivo rgb.txt ja foi criado
     echo "Arquivo rgb.txt ja existe, deseja atualiza-lo [S/n]?"
     read criar 
 fi
