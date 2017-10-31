6521f77aa9405c336d568d381e886d525d3427d2
Jailson Dias <jcd2@cin.ufpe.br>
2017-10-31 13:54:55 -0300

    calibracao

--- Examples/Monocular/calibation.cc
+++ Examples/Monocular/calibation.cc
@@ -21,11 +21,16 @@
 
 
 using namespace std;
+using namespace cv;
+
+const float calibrationSquareDimension = 0.001; // metros
+const float arucoSquareDimension = 0.01; // metros
+const Size chessboardDimension = Size(9, 6);
 
 void createArucoMarkers() {
-    cv::Mat outputMarker;
+    Mat outputMarker;
 
-    cv::Ptr<aruco::Dictionary> markerDictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
+    Ptr<aruco::Dictionary> markerDictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
 
     for (int i = 0; i < 50; i++){
         aruco::drawMarker(markerDictionary, i, 500, outputMarker, 1);
@@ -36,9 +41,76 @@ void createArucoMarkers() {
     }
 }
 
+void createKnownBoardPosition(Size boardSize, float squareEdgeLength, vector<Point3f> &corners){
+    for(int i = 0; i< boardSize.height; i++) {
+        for (int j = 0; j< boardSize.width; j++) {
+            corners.push_back(Point3f(j * squareEdgeLength, i * squareEdgeLength, 0.0f));
+        }
+    }
+}
+
+void getChessboardCorners(vector<Mat> images, vector<vector<Point2f> > &allFoundCorners, bool showResults = false) {
+    for(vector<Mat>::iterator iter = images.begin(); iter != images.end(); iter++) {
+        vector<Point2f> pointBuf;
+        bool found = findChessboardCorners(*iter, Size(9, 6), pointBuf, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_NORMALIZE_IMAGE);
+        if (found) {
+            allFoundCorners.push_back(pointBuf);
+        }
+
+        if (showResults) {
+            drawChessboardCorners(*iter, Size(9,6),pointBuf, found);
+            imshow("Looking for Corners", *iter);
+            waitKey(0);
+        }
+    }
+}
+
 int main() {
-    cout << "teste" << endl;
-    createArucoMarkers();
-    cout << "teste" << endl;
+    // cout << "teste" << endl;
+    // createArucoMarkers();
+    // cout << "teste" << endl;
+    Mat frame;
+    Mat drawToFrame;
+
+    Mat cameraMatrix = Mat::eye(3,3, CV_64F);
+
+    Mat distanceCoefficients;
+
+    vector<Mat> savedImages;
+
+    vector<vector<Point2f> > markerCorners, rejectedCandidates;
+
+    VideoCapture vid(0);
+
+    if(!vid.isOpened()) {
+        return 5;
+    }
+
+    int framesPerSecond = 20;
+
+    namedWindow("Webcam", CV_WINDOW_AUTOSIZE);
+
+    while(true) {
+        if(!vid.read(frame)) {
+            break;
+        }
+
+        vector<Vec2f> foundPoints;
+        bool found = false;
+
+        found = findChessboardCorners(frame, chessboardDimension, foundPoints, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_NORMALIZE_IMAGE);
+        frame.copyTo(drawToFrame);
+        drawChessboardCorners(drawToFrame, chessboardDimension, foundPoints, found);
+
+        if(found) {
+            imshow("Webcam", drawToFrame);
+
+        } else {
+            imshow("Webcam", frame);
+        }
+
+        char character = waitKey(1000/framesPerSecond);
+    }
+
     return 0;
 }
---
+++ testes/v2/slamyolo.py
@@ -0,0 +1,52 @@
+import glob
+import datetime
+tempo = datetime.datetime.now().timestamp() # pega o tempo atual e coloca ele no formato timestamp
+
+print ("lendo imagens")
+
+# fotos = glob.glob("rgb/*.png") # lendo fotos da pasta rgb
+# print ("Existe " + str(len(fotos)) + " imagens na pasta rgb")
+# fotos.sort() # ordenando as fotos para coloca-las em ordem crescente no arquivo rgb.txt
+timestamp = 0.003037 # tempo entre cada frame
+
+r = open("rgb.txt", "r") # abrindo o arquivo rgb.txt ou criando-o caso ele nao exista
+saida = open("saida.txt", "r") # abrindo o arquivo rgb.txt ou criando-o caso ele nao exista
+f = open("rgbm.txt", "w")
+f.write("# color images\n")
+f.write("# color images\n")
+f.write("# timestamp filename\n")
+
+yolo = saida.readlines()
+index = 0
+vezesyolo = 0
+
+fotos = r.readlines()
+objs = ""
+for i, foto in enumerate(fotos[2:]): # colocando o timestamp e o endereco das imagens no arquivo rgb.txt
+    if (index < len(yolo)):
+        line = yolo[index]
+    # print (line)
+    # print (foto)
+    vezesslam = 0
+    while (index < len(yolo) and int(line.split(' ')[0]) == int(foto.split('/')[1].split('.')[0])):
+        if (vezesslam == 0):
+            objs = ""
+        objs += " " + (' '.join(line.split(' ')[1:]).split('\n')[0])
+        index += 1
+        vezesslam += 1
+        vezesyolo = 0
+        if (index < len(yolo)):
+            line = yolo[index]
+    if (vezesyolo < 5):
+        vezesyolo += 1
+        f.write(foto.split('\n')[0] + objs + '\n')
+    else:
+        f.write(foto.split('\n')[0] + '\n')
+        
+
+
+f.close()
+saida.close()
+r.close() # fechando o arquivo
+
+print ("operação finalizada")
